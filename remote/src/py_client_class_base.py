63a4e2267d1b2e9f54cd5ec088e6493f19f2b797
Ivan Gao <20075113ghl@sina.com>
2019-06-06 20:57:08 +0800

    修复了读取协议时读取部分的bug

--- remote/src/py_client_class_base.py
+++ remote/src/py_client_class_base.py
@@ -7,8 +7,12 @@
             self.s.send(snd_data.get_buf())
             len_bytes = self.s.recv(size_of_ulong())
             data_len = to_ulong(len_bytes)
-            if(data_len > max_rcv_len ):raise
+            if (data_len > max_rcv_len): raise
             data_buf = self.s.recv(data_len)
+            while (len(data_buf) != data_len):
+                this_buf = self.s.recv(data_len - len(data_buf))
+                data_buf += this_buf
+
             recv_data = lt_data_t()
             recv_data.from_len_buf(data_len, data_buf)
             return recv_data
